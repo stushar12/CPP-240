0db5111b2c769b5cd4178090a0b2b97ea8586fc1
Tushar Saxena <stushar345@gmail.com>
2021-06-06 01:38:26 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -1,16 +1,20 @@
-int maxArea(const vector<int>& height) {
+int maxArea(const vector<int>& height) 
+{
   int leader = 0;
-  for (int a = 0, z = height.size() - 1; a < z;) 
+  int low=0,high=height.size()-1;
+  while(low<high)
   {
-    int curr_height = min(height[a], height[z]);
-    int curr_area = (z - a) * curr_height;
+    int curr_height = min(height[low], height[high]);    //water can fill upto min height
+    int curr_area = (high-low) * curr_height;             //total water = difference between two pillars * min height
     if (curr_area > leader) 
         leader = curr_area;
 
-    // move both bars until we find something taller than the current height
-    for (; a < z && height[a] <= curr_height; a++) {}
-    for (; a < z && height[z] <= curr_height; z--) {}
+    if(height[low]<height[high])  //change the pillar whose height is less out of the two because lower height pillar cannot yield more result than the previous smaller one
+    low++;
+    else
+    high--;
   }
 
   return leader;
+}
 }
